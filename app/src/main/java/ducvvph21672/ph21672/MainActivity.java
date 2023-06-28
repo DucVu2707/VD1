9d45d443564b4201391076914c4ae30a3ed37b95
vuduc <ducvvph21672@fpt.edu.vn>
2023-06-28 13:10:56 +0700

    Initial commit

---
+++ app/src/main/java/ducvvph21672/ph21672/MainActivity.java
@@ -0,0 +1,14 @@
+package ducvvph21672.ph21672;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/ducvvph21672/ph21672/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ducvvph21672.ph21672;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
